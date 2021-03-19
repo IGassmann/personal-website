75d403165068331e15e0a681d9ba315cde34e9a3
Igor Gassmann <igor@igassmann.me>
2021-03-19 18:39:12 -0300

    fix: remove 1:1 twitter image

--- src/site.config.js
+++ src/site.config.js
@@ -45,14 +45,8 @@ export default {
         alt: 'Igor Gassmann',
       },
       {
-        url: `${siteOrigin}/twitter-image-2-1.png`,
+        url: `${siteOrigin}/twitter-image.png`,
         width: 1200,
-        height: 600,
-        alt: 'Igor Gassmann',
-      },
-      {
-        url: `${siteOrigin}/twitter-image-1-1.jpg`,
-        width: 600,
         height: 600,
         alt: 'Igor Gassmann',
       },

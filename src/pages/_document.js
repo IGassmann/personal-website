2d26ba525b763f4c9d610194c4a957134433471f
Igor Gassmann <igor@igassmann.me>
2021-03-20 19:47:35 -0300

    feat: add favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en-US">
         <Head>
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <link rel="icon" type="image/x-icon" sizes="16x16 32x32" href="/favicons/favicon.ico" />
+          <link rel="icon" sizes="192x192" href="/favicons/favicon-192.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/favicon-180-precomposed.png" />
+          <meta name="msapplication-TileColor" content="#190950" />
+          <meta name="msapplication-TileImage" content="/favicons/favicon-114-precomposed.png" />
+          <link rel="manifest" href="/manifest.json" />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}

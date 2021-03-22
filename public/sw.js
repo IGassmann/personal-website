39695889534f47e1365a77d01f91bbefbb74c947
Igor Gassmann <igor@igassmann.me>
2021-03-22 16:50:47 -0300

    fix: window is not defined in service workers

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,8 @@
-if ('caches' in window) {
-  caches.keys()
-    .then(keys => {
-      return Promise.all(keys.map(key => caches.delete(key)));
-    })
-}
-
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.getRegistrations()
-      .then(registrations => {
-        for (let registration of registrations) {
-          registration.unregister().then(hasUnregistered => {console.log('Unregistered: ', hasUnregistered);});
-        }
-        window.location.reload();
-      });
-  });
+  navigator.serviceWorker.getRegistrations()
+    .then(registrations => {
+      for (let registration of registrations) {
+        registration.unregister().then(hasUnregistered => {console.log('Unregistered: ', hasUnregistered);});
+      }
+    });
 }

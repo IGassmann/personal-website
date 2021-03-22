eea2cd84462a5da1c153af711a9918bbab5c2964
Igor Gassmann <igor@igassmann.me>
2021-03-22 16:14:56 -0300

    fix: obsolete service worker doesn't let website load

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,23 @@
 import GoogleTagManager from '@/components/GoogleTagManager/GoogleTagManager';
 import DefaultLayout from '@/layouts/DefaultLayout';
 import { DefaultSeo } from 'next-seo';
-import React from 'react';
+import React, { useEffect } from 'react';
 import '@/styles/globals.scss';
 import siteConfig from '@/site.config';
 
 export default function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || DefaultLayout;
+
+  // Unregister previous website version service worker.
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+        for(let registration of registrations) {
+          registration.unregister();
+        }
+      });
+    }
+  }, [])
 
   return (
     <GoogleTagManager>

4dc8933432ef08fa370b8668f40c1a4c1207b3e1
Igor Gassmann <igor@igassmann.me>
2021-03-23 15:47:47 -0300

    refactor: use classnames package

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
+import classNames from 'classnames/bind';
 import styles from './Pagination.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Pagination = ({ currentPage, numberOfPages }) => {
   const hasPreviousPage = currentPage !== 0;
@@ -12,12 +15,12 @@ const Pagination = ({ currentPage, numberOfPages }) => {
     <div className={styles.pagination}>
       <div>
         <Link rel="prev" href={previousPagePath}>
-          <a className={`${styles.link} ${!hasPreviousPage && styles.disable}`}>← PREV</a>
+          <a className={cx('link', {disable: !hasPreviousPage})}>← PREV</a>
         </Link>
       </div>
       <div>
         <Link rel="next" href={nextPagePath}>
-          <a className={`${styles.link} ${!hasNextPage && styles.disable}`}>NEXT →</a>
+          <a className={cx('link', {disable: !hasNextPage})}>NEXT →</a>
         </Link>
       </div>
     </div>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,47 +1,39 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import classNames from 'classnames/bind';
 import { useMediaQuery, useSiteMetadata } from '@/hooks';
 import styles from './Profile.module.scss';
 
+const cx = classNames.bind(styles);
+
 const Profile = ({ isInline }) => {
-  const {
-    subtitle,
-    profilePicture,
-    layoutBreakpointSmall,
-    profile: profileStyle,
-    inline,
-    text,
-    title,
-    profilePictureLink
-  } = styles;
-
-  const isPageWide = useMediaQuery(`(min-width: ${layoutBreakpointSmall})`)
   const { profile } = useSiteMetadata();
+  const isPageWide = useMediaQuery(`(min-width: ${styles.layoutBreakpointSmall})`)
 
-
+  const inlineClass = { inline: isInline || !isPageWide };
   const imageSize = isInline ? 64 : 80;
 
   return (
-    <div className={`${profileStyle} ${isInline || !isPageWide ? inline : ''}`}>
+    <div className={cx('profile', inlineClass)}>
       <Link href="/">
-        <a className={`${profilePictureLink} ${isInline || !isPageWide ? inline : ''}`}>
+        <a className={cx('profilePictureLink', inlineClass)}>
           <Image
             src={profile.picture}
-            className={`${profilePicture} ${isInline || !isPageWide ? inline : ''}`}
+            className={cx('profilePicture', inlineClass)}
             width={imageSize}
             height={imageSize}
             alt={profile.name}
           />
         </a>
       </Link>
-      <div className={`${text} ${isInline || !isPageWide ? inline : ''}`}>
-        <h1 className={`${title} ${isInline || !isPageWide ? inline : ''}`}>
+      <div className={cx('text', inlineClass)}>
+        <h1 className={cx('title', inlineClass)}>
           <Link href="/">
             <a>{profile.name}</a>
           </Link>
         </h1>
-        <p className={`${subtitle} ${isInline || !isPageWide ? inline : ''}`}>{profile.bio}</p>
+        <p className={cx('subtitle', inlineClass)}>{profile.bio}</p>
       </div>
     </div>
   );

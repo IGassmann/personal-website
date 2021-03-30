ece105f627b1074c2559d1698c6625cab3af5d26
Igor Gassmann <igor@igassmann.me>
2021-03-30 20:13:24 -0300

    refactor: move logic to index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,66 @@
-import { getStaticProps } from '@/pages/page/[blogPageIndex]'
-import Index from '@/pages/page/[blogPageIndex]'
+import React from 'react';
+import fs from 'fs';
+import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
+import Pagination from '@/components/Pagination';
+import PostFeed from '@/components/PostFeed';
+import { getAllPosts } from '@/lib/posts';
+import generateRSS from '@/lib/generateRSS';
 
-export { getStaticProps }
-export default Index
+const BlogPage = ({ posts, currentPage, numberOfPages, origin }) => {
+  const router = useRouter()
+  const pageTitle = currentPage > 0 ? `Posts - Page ${currentPage}` : '';
+
+  return (
+    <>
+      <NextSeo
+        title={pageTitle}
+        openGraph={{
+          title: pageTitle,
+          url: `${origin}${router.asPath}`,
+        }}
+      />
+      <PostFeed posts={posts} />
+      <Pagination
+        currentPage={currentPage}
+        numberOfPages={numberOfPages}
+      />
+    </>
+  );
+}
+
+export default BlogPage;
+
+export async function getStaticProps({ params }) {
+  const pageIndexParam = params?.blogPageIndex;
+  const pageIndex = parseInt(pageIndexParam || '0')
+
+  const { default: { postsPerPage, origin } } = await import('@/site.config')
+
+  const posts = getAllPosts([
+    'title',
+    'summary',
+    'category',
+    'publishedAt',
+    'slug',
+  ]);
+
+  // For pagination and ordering
+  const numberOfPages = Math.ceil(posts.length / postsPerPage)
+  const paginatedPosts = posts.slice(
+    pageIndex * postsPerPage,
+    (pageIndex + 1) * postsPerPage
+  )
+
+  const rss = generateRSS(posts);
+  fs.writeFileSync('./public/rss.xml', rss);
+
+  return {
+    props: {
+      posts: paginatedPosts,
+      currentPage: pageIndex,
+      numberOfPages,
+      origin,
+    },
+  };
+}
--- src/pages/page/[blogPageIndex].js
+++ src/pages/page/[blogPageIndex].js
@@ -1,69 +1,10 @@
 import React from 'react';
-import * as fs from 'fs';
-import { NextSeo } from 'next-seo';
-import { useRouter } from 'next/router';
-import PostFeed from '@/components/PostFeed';
-import Pagination from '@/components/Pagination';
+import { getStaticProps } from '@/pages';
+import BlogPage from '@/pages';
 import { getAllPosts } from '@/lib/posts';
-import generateRSS from '@/lib/generateRSS';
 
-const BlogPagePage = ({ posts, currentPage, numberOfPages, origin }) => {
-  const router = useRouter()
-  const pageTitle = currentPage > 0 ? `Posts - Page ${currentPage}` : '';
-
-  return (
-    <>
-      <NextSeo
-        title={pageTitle}
-        openGraph={{
-          title: pageTitle,
-          url: `${origin}${router.asPath}`,
-        }}
-      />
-      <PostFeed posts={posts} />
-      <Pagination
-        currentPage={currentPage}
-        numberOfPages={numberOfPages}
-      />
-    </>
-  );
-}
-
-export default BlogPagePage;
-
-export async function getStaticProps({ params }) {
-  const pageIndexParam = params?.blogPageIndex;
-  const pageIndex = parseInt(pageIndexParam || '0')
-
-  const { default: { postsPerPage, origin } } = await import('@/site.config')
-
-  const posts = getAllPosts([
-    'title',
-    'summary',
-    'category',
-    'publishedAt',
-    'slug',
-  ]);
-
-  // For pagination and ordering
-  const numberOfPages = Math.ceil(posts.length / postsPerPage)
-  const paginatedPosts = posts.slice(
-    pageIndex * postsPerPage,
-    (pageIndex + 1) * postsPerPage
-  )
-
-  const rss = generateRSS(posts);
-  fs.writeFileSync('./public/rss.xml', rss);
-
-  return {
-    props: {
-      posts: paginatedPosts,
-      currentPage: pageIndex,
-      numberOfPages,
-      origin,
-    },
-  };
-}
+export default BlogPage;
+export { getStaticProps }
 
 export async function getStaticPaths() {
   const { default: { postsPerPage } } = await import('@/site.config')

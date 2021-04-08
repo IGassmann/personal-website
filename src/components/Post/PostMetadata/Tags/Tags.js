3d0a97915dc6bc9c7fa319283202d0822efe375f
Igor Gassmann <igor@igassmann.me>
2021-04-08 10:21:23 -0300

    fix: remove broken tag links

--- src/components/Post/PostMetadata/Tags/Tags.js
+++ src/components/Post/PostMetadata/Tags/Tags.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
 import styles from './Tags.module.scss';
 
 const Tags = ({ tags }) => (
-  <div className={styles.tags}>
-    <ul className={styles['tags__list']}>
+  <div>
+    <ul className={styles.tagList}>
       {tags && tags.map((tag, index) => (
-        <li className={styles['tags__list-item']} key={tags[index]}>
-          <Link href={`/tag/${tag}`}>
-            <a className={styles['tags__list-item-link']}>#{tags[index]}</a>
-          </Link>
-        </li>
+        <li className={styles.tagListItem} key={index}>#{tag}</li>
       ))}
     </ul>
   </div>

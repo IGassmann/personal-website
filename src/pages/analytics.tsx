e768e52a78f6f6ac01ca1abffbe162c022219660
Igor Gassmann <igor@igassmann.me>
2021-04-21 20:20:15 -0300

    refactor: remove unused props from analytics page

--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
-import matter from 'gray-matter';
-import { join } from 'path';
-import fs from 'fs';
 import SingleColumnLayout from '@/layouts/SingleColumnLayout';
 import Analytics from '@/components/Analytics'
 
@@ -33,17 +30,9 @@ export default AnalyticsPage;
 export const getStaticProps: GetStaticProps = async () => {
   const { default: { origin } } = await import('@/site.config')
 
-  const aboutPath = join(process.cwd(), 'src', 'content', 'about.md');
-  const fileContent = fs.readFileSync(aboutPath, 'utf8');
-  const { content, data } = matter(fileContent);
-
   return {
     props: {
       origin,
-      about: {
-        ...data,
-        content,
-      },
     },
   };
 }

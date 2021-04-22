69a216ee068c13043d5df8e84862296dca0b68dc
Igor Gassmann <igor@igassmann.me>
2021-04-22 18:29:31 -0300

    rever: "refactor: upgrade react-markdown"
    This reverts commit 13e23280d49c9fc0b34b7b5f77caf72eb7624541. remark-directive isn't working on the latest version.

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import directive from 'remark-directive';
-import markdownComponents from '@/lib/markdownComponents';
+import markdownRenderers from '@/lib/markdownRenderers';
 import PostMetadata from './PostMetadata';
 import styles from './Post.module.scss';
 
@@ -9,9 +9,7 @@ const Post = ({ post }) => (
   <article className={styles.post}>
     <h1 className={styles.title}>{post.title}</h1>
     <PostMetadata publishedAt={post.publishedAt} tags={post.tags}/>
-    <ReactMarkdown plugins={[directive]} className={styles.body} components={markdownComponents}>
-      {post.content}
-    </ReactMarkdown>
+    <ReactMarkdown plugins={[directive]} className={styles.body} renderers={markdownRenderers} children={post.content} />
   </article>
 );
 

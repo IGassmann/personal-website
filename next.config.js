9e863c8832bdf06087dac88bb08779d1505bc2a4
Igor Gassmann <igor@igassmann.me>
2021-05-03 14:25:12 -0300

    feat: add security HTTP headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const { createSecureHeaders } = require("next-secure-headers");
+const defaultSrcCSPDirectives = ["'self'", "https://igassmann.me", "https://*.igassmann.me"]
+
 module.exports = {
   future: { webpack5: true },
   reactStrictMode: true,
@@ -8,5 +11,22 @@ module.exports = {
     }
 
     return config;
-  }
+  },
+  async headers() {
+    return [{
+      source: "/(.*)",
+      headers: createSecureHeaders({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: defaultSrcCSPDirectives,
+            imgSrc: [...defaultSrcCSPDirectives, "data:"],
+            scriptSrc: [...defaultSrcCSPDirectives, "'unsafe-eval'"],
+            styleSrc: [...defaultSrcCSPDirectives, "'unsafe-inline'"],
+            frameSrc: [...defaultSrcCSPDirectives, "https://*"]
+          },
+        },
+        referrerPolicy: "no-referrer-when-downgrade",
+      }),
+    }];
+  },
 };

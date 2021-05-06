385f11f5d60039700619eb38185b78b98b89c6b2
Igor Gassmann <igor@igassmann.me>
2021-05-06 14:08:19 -0300

    refactor: migrate Analytics to Tailwind CSS

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import styles from './Analytics.module.scss';
 
 const Analytics = () => (
   <>
     <iframe plausible-embed
             src="https://plausible.io/share/igassmann.me?auth=d1GNfDh72M25z1HHp8vqF&amp;embed=true&amp;theme=dark&amp;background=transparent"
             loading="lazy"
-            className={styles.analytics}/>
+            className="border-none w-[1px] min-w-full h-[1600px]"/>
     <script async src="https://stats.igassmann.me/js/embed.host.js"/>
   </>
 );

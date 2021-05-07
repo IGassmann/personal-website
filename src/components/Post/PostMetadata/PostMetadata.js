396c29f8bd7d4a7d3ff9662a3c790658b39bede1
Igor Gassmann <igor@igassmann.me>
2021-05-07 14:13:33 -0300

    refactor: migrate PostMetadata to Tailwind CSS

--- src/components/Post/PostMetadata/PostMetadata.js
+++ src/components/Post/PostMetadata/PostMetadata.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import dayjs from 'dayjs';
 import Tags from './Tags';
-import styles from './PostMetadata.module.scss';
 
 const PostMetadata = ({ publishedAt, tags }) => (
-  <div className={styles.postMetadata}>
-    <time className={styles.publishedDate} dateTime={publishedAt}>
+  <div className="flex flex-wrap place-content-between mt-l mb-xxl">
+    <time className="mr-l pt-l text-body-text text-opacity-80" dateTime={publishedAt}>
       {dayjs(publishedAt).format('D MMM YYYY')}
     </time>
     {tags && <Tags tags={tags} />}

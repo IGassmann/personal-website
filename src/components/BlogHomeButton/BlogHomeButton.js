21ab640dbad8d18812a07c6db4cce56ef54af5f2
Igor Gassmann <igor@igassmann.me>
2021-05-09 21:29:17 -0300

    fix: sticky position of BlogHomeButton

--- src/components/BlogHomeButton/BlogHomeButton.js
+++ src/components/BlogHomeButton/BlogHomeButton.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const BlogHomeButton = () => {
   return (
     <Link href="/">
-      <a className="block w-max h-[42px] leading-[42px] px-xl text-center text-secondary border border-secondary-dark rounded-[20px] mx-auto hover:focus:text-primary md:sticky md:m-0 md:top-6 md:left-6">All Posts</a>
+      <a className="block w-max h-[42px] leading-[42px] px-xl text-center text-secondary border border-secondary-dark rounded-[20px] mx-auto hover:focus:text-primary md:sticky md:m-0 md:top-[30px] md:left-[30px]">All Posts</a>
     </Link>
   );
 }

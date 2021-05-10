f1a357a27f4498797c82e8bb9391b31febb4b3fd
Igor Gassmann <igor@igassmann.me>
2021-05-09 21:26:13 -0300

    refactor: migrate Footer to Tailwind CSS

--- src/layouts/SingleColumnLayout/Footer/Footer.js
+++ src/layouts/SingleColumnLayout/Footer/Footer.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Profile from 'components/Profile';
-import styles from './Footer.module.scss';
 
 const Footer = () => (
-  <footer className={styles.footer}>
+  <footer className="max-w-[640px] border-t border-primary mx-auto pt-xxl">
     <Profile isInline/>
   </footer>
 );

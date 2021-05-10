74d243135380bc6f7df375db2d47a861a5526d41
Igor Gassmann <igor@igassmann.me>
2021-05-09 21:07:28 -0300

    refactor: migrate Sidebar to Tailwind CSS

--- src/layouts/DefaultLayout/Sidebar/Sidebar.js
+++ src/layouts/DefaultLayout/Sidebar/Sidebar.js
@@ -3,13 +3,12 @@ import Menu from '@/layouts/DefaultLayout/Sidebar/Menu';
 import SocialLinks from '@/layouts/DefaultLayout/Sidebar/SocialLinks';
 import Profile from '@/components/Profile';
 import { useSiteMetadata } from '@/hooks';
-import styles from './Sidebar.module.scss';
 
 const Sidebar = () => {
   const { profile: { socialLinks }, menu } = useSiteMetadata();
 
   return (
-    <aside className={styles.sidebar}>
+    <aside className="relative mb-xxxl sm:border-r border-secondary">
       <Profile isHeading />
       <Menu menu={menu} />
       <SocialLinks socialLinks={socialLinks} />

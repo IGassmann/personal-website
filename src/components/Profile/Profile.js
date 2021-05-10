ede077c1620b3c7efa24347e6aa8abad554bb826
Igor Gassmann <igor@igassmann.me>
2021-05-10 13:18:26 -0300

    refactor: migrate Profile to Tailwind CSS

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import classNames from 'classnames/bind';
 import { useMediaQuery, useSiteMetadata } from '@/hooks';
-import styles from './Profile.module.scss';
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../../tailwind.config'
 
-const cx = classNames.bind(styles);
+const fullConfig = resolveConfig(tailwindConfig)
 
 const Profile = ({ isInline, isHeading }) => {
   const { profile } = useSiteMetadata();
-  const isPageWide = useMediaQuery(`(min-width: ${styles.layoutBreakpointSmall})`)
 
-  const inlineClass = { inline: isInline || !isPageWide };
+  const isPageWide = useMediaQuery(`(min-width: ${fullConfig.theme.screens.sm})`)
+  isInline = isInline || !isPageWide
+
   const imageSize = isInline ? 64 : 80;
 
   const TitleTag = isHeading ? 'h1' : 'span'
 
   return (
-    <div className={cx('profile', inlineClass)}>
+    <div className={`flex flex-col w-full ${isInline ? 'flex-row' : ''}`}>
       <Link href="/">
-        <a className={cx('profilePictureLink', inlineClass)}>
+        <a className={`w-[64px] ${isInline ? 'mr-l' : ''}`}>
           <Image
             src={profile.picture}
-            className={cx('profilePicture', inlineClass)}
+            className="inline-block rounded-full bg-clip-padding sm:w-[80px]"
             width={imageSize}
             height={imageSize}
             alt={profile.name}
           />
         </a>
       </Link>
-      <div className={cx('text', inlineClass)}>
-        <TitleTag className={cx('title', inlineClass)}>
+      <div className={isInline ? 'flex-1' : ''}>
+        <TitleTag className={`text-h3 my-l ${isInline ? 'my-0' : ''}`}>
           <Link href="/">
-            <a>{profile.name}</a>
+            <a className="text-primary">{profile.name}</a>
           </Link>
         </TitleTag>
-        <p className={cx('subtitle', inlineClass)}>{profile.tagline}</p>
+        <p className="text-body-text-color text-opacity-80 m-0">{profile.tagline}</p>
       </div>
     </div>
   );

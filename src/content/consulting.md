8cccd16c8421a9624ad470b8a19c4ac428cb5400
Igor Gassmann <igor@igassmann.me>
2023-02-18 16:23:23 +0200

    feat(content): add consulting page

---
+++ src/content/consulting.md
@@ -0,0 +1,11 @@
+---
+title: Consulting
+---
+
+## Consulting
+
+I offer consulting services for startups and small businesses. I can help you with your software
+development needs, from product design to infrastructure.
+
+If you're interested in working with me, please [contact me](mailto:igor@igassmann.me) to discuss
+your needs.
---
+++ src/pages/consulting.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { GetStaticProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
+import matter from 'gray-matter';
+import { join } from 'path';
+import fs from 'fs';
+import ReactMarkdown from 'react-markdown';
+
+type ConsultingPageProps = {
+  origin: string;
+  consulting: { title: string; content: string };
+};
+
+const ConsultingPage: NextPage<ConsultingPageProps> = ({ origin, consulting }) => {
+  const { title, content } = consulting;
+
+  const router = useRouter();
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description="Consulting Igor Gassmann"
+        openGraph={{
+          url: `${origin}${router.asPath}`,
+          title,
+        }}
+      />
+      <div className="-mt-l">
+        <ReactMarkdown>{content}</ReactMarkdown>
+      </div>
+    </>
+  );
+};
+
+export default ConsultingPage;
+
+export const getStaticProps: GetStaticProps<ConsultingPageProps> = async () => {
+  const {
+    default: { origin },
+  } = await import('@/site.config');
+
+  const consultingPath = join(process.cwd(), 'src', 'content', 'consulting.md');
+  const file = fs.readFileSync(consultingPath, 'utf8');
+  const parsedFile = matter(file);
+  const metadata: { [key: string]: unknown } = parsedFile.data;
+  const { content } = parsedFile;
+
+  return {
+    props: {
+      origin,
+      consulting: {
+        ...metadata,
+        content,
+      } as { title: string; content: string },
+    },
+  };
+};
